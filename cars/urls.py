9c93f4139cf4e285b066fdab9d04dd7b645a83a7
Awais Ishtiaq <awais.ishtiaq08@gmail.com>
2025-08-01 05:50:42 +0500

    making car detail pages and show features in that page

--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import cars
+from .views import cars, car_detail
 
 
 
 urlpatterns = [
     path('', cars, name='cars'),
+    path('<int:id>', car_detail, name='car_detail'),
+
 
 
 ]
